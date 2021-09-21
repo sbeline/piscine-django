c9e7884950aca36ab114d6aec037ac3c9bcacd34
sbeline <sbeline@student.42.fr>
2021-09-21 10:27:24 +0200

    D04 done setup of django

--- D04/ex00/d04/ex00/apps.py
+++ D04/ex00/d04/ex00/apps.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.apps import AppConfig
 
 
 class Ex00Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'ex00'
